0a056c45b7bbca7d33692c18d119aff8086b00ef
phort6n2 <158865829+phort6n2@users.noreply.github.com>
2025-12-30 14:33:10 -0700

    Update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({
+  datasourceUrl: process.env.DATABASE_URL
+})
 import bcrypt from 'bcryptjs'
 
 async function main() {
