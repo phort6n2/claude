63e25447d56fa8a3cd427c52688a11cada8316a7
phort6n2 <158865829+phort6n2@users.noreply.github.com>
2025-12-30 13:59:28 -0700

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,8 +4,14 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+// Only instantiate Prisma if we're not in build mode
+export const prisma = 
+  process.env.NEXT_PHASE === 'phase-production-build'
+    ? {} as PrismaClient
+    : (globalForPrisma.prisma ?? new PrismaClient())
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production' && process.env.NEXT_PHASE !== 'phase-production-build') {
+  globalForPrisma.prisma = prisma
+}
 
 export default prisma
